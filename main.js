8679b04b0ff4ea82b13c705c806f2a3635971629
Jadslog <121417622+Jadslog@users.noreply.github.com>
2023-05-30 23:04:10 -0600

    main.js code

--- main.js
+++ main.js
@@ -23,19 +23,65 @@ let allPosts = [
   },
 ];
 
-
 const searchIcon = document.querySelector("header.main-header .search-icon");
 const searchResults = document.querySelector("header.main-header .search-results");
 const searchInput = document.querySelector(".search-results .search-input");
+const searchTitle = document.querySelector(".search-results .search-title");
+const resultsContainer = document.querySelector(".results-container")
 
-let filteredPosts
+let filteredPosts = [];
 
 searchIcon.addEventListener ("click", () => {
     searchResults.classList.toggle("active");
 });
 
+const populateSearchResults = () => {
+    const ul = document.createElement("ul");
+
+    filteredPosts.forEach(p => {
+      const li = document.createElement("li")
+      const a = document.createElement("a");
+      a.href = "#";
+      a.textContent = p.title;
+      li.appendChild(a);
+      ul.appendChild(li);
+    });
+
+    resultsContainer.innerHTML = "";
+    resultsContainer.appendChild(ul);
+}
+
 searchInput.addEventListener("input", (e) => {
     const query = e.target.value.toLowerCase();
 
-    if (query )
+    if (query) {
+      filteredPosts = allPosts.filter((post) => {
+          const title = post.title.toLowerCase();
+          return title.includes(query);
+      }); 
+
+      populateSearchResults();
+      searchTitle.textContent = "Search Results";
+    } else {
+      searchTitle.textContent = "Featured Posts";
+      resultsContainer.innerHTML = `<ul>
+    <li>
+      <a href="#"
+        >The Science of Sleep: Understanding the Importance of Rest</a
+      >
+    </li>
+    <li>
+      <a href="#"
+        >Unleashing Your Leadership Potential: Essential Skills for
+        Today's Leaders</a
+      >
+    </li>
+    <li>
+      <a href="#"
+        >Mastering Personal Finance: Budgeting, Saving, and Investing
+        Strategies</a
+      >
+    </li>
+  </ul>`
+    }
 });
